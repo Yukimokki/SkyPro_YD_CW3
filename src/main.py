a0432b36ea9e1ca4f88d9f583f5ffae2fb6cacf1
Yukimokki <yana.domnina@gmail.com>
2024-06-02 14:14:56 +0300

    second develop commit

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
-from trans_class import *
+from utils import *
 
-trans_load()
+def main():
+    num_operations = 5
+    operations = trans_dict(trans_load(num_operations))
+
+    for operation in operations:
+        date = trans_dict_format_date(operation["date"])
+        trans_from = origin_format(operation['from'])
+        trans_to = dest_format(operation['to'])
+
+        print(f"""
+        {date} {operation['description']}
+        {trans_from} -> {trans_to}
+        {operation['summ']} {operation['currency']}""")
+
+main()
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,86 @@
 import json
-def load_json():
+
+
+def trans_load(num_operations):
     """
-    загружает банковский отчет
+    загружает банковский отчет и выбирает 5 последних успешных операции
     """
     with open('operations.json', 'r', encoding='utf-8') as file:
-        operations  =  json.load(file)
-        return operations
+        trans_list = [x for x in json.load(file) if x and x["state"] == "EXECUTED"]
+        trans_list.sort(key=lambda k: str(k.get('date')), reverse=True)
+        return trans_list[0:int(num_operations)]
 
-#print(load_json())
+
+def trans_dict(operations_list):
+    """
+   выбирает нужные значения для формирования выписки
+   """
+
+    transactions = operations_list
+    trans_list_items = []
+    for trans in transactions:
+        transaction = {"state": trans["state"], "date": trans["date"], "description": trans['description'],
+                       "from": trans.get("from"), "to": trans["to"], "summ": trans["operationAmount"]["amount"],
+                       "currency": trans["operationAmount"]["currency"]["name"], "id": trans['id']}
+        trans_list_items.append(transaction)
+
+    return trans_list_items
+
+
+def trans_dict_format_card(card_number):
+    """
+    форматирует номер карты
+    """
+    card_number_list = (card_number[:-12:1], " ", card_number[-12:-10:1], "** **** ", card_number[-5:-1:1])
+    card_number_format = "".join(card_number_list)
+    return card_number_format
+
+
+def origin_format(origin):
+    """
+    выбирает функцию форматирования просихождения операции
+    """
+    if origin and origin[0:4] == 'Счет':
+        trans_from = trans_dict_format_acc(origin)
+    elif origin and origin[0:4] != 'Счет':
+        trans_from = trans_dict_format_card(origin)
+    elif origin == None:
+        trans_from = 'Нет'
+    return trans_from
+
+
+def dest_format(destination):
+    """
+    выбирает функцию для форматирования назначения операции
+    """
+    if destination[0:4] == 'Счет':
+        trans_to = trans_dict_format_acc(destination)
+    elif destination[0:4] != 'Счет':
+        trans_to = trans_dict_format_card(destination)
+    return trans_to
+
+
+def trans_dict_format_acc(account):
+    """
+    форматирует значения номера счёта
+    """
+    return 'Счет **' + account[-4:]
+
+
+def trans_dict_format_date(trans_date):
+    """
+    выводит правильный формат даты
+    """
+    date = trans_date.split('T')
+    date_items = date[0].split('-')
+    date_list = [date_items[2], date_items[1], date_items[0]]
+    date_format = ".".join(date_list)
+    return date_format
+
+
+def trans_printout():
+    """
+    выполняет печатть необходимого числа транзакций
+    * не выполнена
+    """
+    pass
